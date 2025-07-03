e852f4c98eab2ff35bc30c5835543d80bf1ec085
Eze-Israel <ezeisraeloluoma123@gmail.com>
2025-07-03 17:27:08 +0100

    fixed and made some adjustments in color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,51 +1,3 @@
-// import React from 'react';
-// import Link from 'next/link';
-// import {  FaLinkedin, FaGithub,FaWhatsapp} from 'react-icons/fa';
-
-// const Footer = () => {
-//    const  date = new Date().getFullYear();
-//   return (
-//     <div className='footer'>
-//     <Link href="/" className="name"> <b className='text-green-500 '>Is</b>rael</Link>
-//       <div className='navigation'>
-//        <ul className='nav-menu-show'>
-//       <Link href="/" > <li className='footlist'>Home</li></Link> 
-//       <Link href="#about" > <li className='footlist'>About</li></Link> 
-//       <Link href="#skills">  <li className='footlist'>Skills</li></Link> 
-//       <Link href="#projects" > <li className='footlist'>Projects</li></Link> 
-//       <Link href="#contact" > <li className='footlist'>Contact</li></Link> 
-//       </ul>
-//      </div>
-
-     
-       
-//      <div className='socials'>
-//      <FaLinkedin className='linked'/>
-//       <a  href="https://linkedin.com/in/israel-eze-ab471a305" rel="norerrer">
-//           </a>
-
-//           <a href="https://github.com/Eze-Israel" target='_blank'>
-
-//      <FaGithub className="hub" /></a>
-//      <a href="https://wa.me/+2347061033459" rel="norerrer" target='_blank'>
-//       <FaWhatsapp className='wats' />
-//       </a>
-//       </div>
-
-//       <div className='copyright'> <p > &copy; {date} <b style={{
-//     color:"orange"
-//      }}> Eze Israel O.</b> All Rights Reserved.</p></div>
-//       </div>
-//   )
-// }
-
-
-
-// export default Footer
-
-
-
-
 import React from 'react';
 import Link from 'next/link';
 import { FaLinkedin, FaGithub, FaWhatsapp } from 'react-icons/fa';
@@ -58,7 +10,7 @@ const Footer = () => {
         <div>
           <h2 className="text-2xl font-bold text-yellow-600 mb-3">Eze Israel Oluomachukwu</h2>
           <p className="text-sm leading-relaxed">
-            A passionate Web Developer & Designer focused on crafting clean, user‑friendly experiences. Let's build something amazing together.
+            A passionate Web Developer & Designer focused on crafting clean, user‑friendly experiences. Let&apos;s build something amazing together.
           </p>
         </div>
 
